c2ab4bef072c2f304d5f4c79f648d91787fae517
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-06 19:27:49 +0200

    stream ages added

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+def stream_user_ages():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",  # Update if needed
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data")
+
+    for (age,) in cursor:
+        yield age  # Yield age one by one
+
+    cursor.close()
+    connection.close()
+
+
+def calculate_average_age():
+    total_age = 0
+    count = 0
+
+    for age in stream_user_ages():
+        total_age += age
+        count += 1
+
+    if count == 0:
+        print("Average age of users: 0")
+    else:
+        average = total_age / count
+        print(f"Average age of users: {average:.2f}")
+
