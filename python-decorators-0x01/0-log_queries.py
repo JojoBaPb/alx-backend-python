b710c8affa730414fe16b81f1a052a37ea022ac9
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-10 12:04:44 +0200

    date time param

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,19 +1,20 @@
 import sqlite3
 import functools
+from datetime import datetime
 
-# Decorator to log SQL queries
+# Decorator to log SQL queries with timestamp
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get('query') if 'query' in kwargs else args[0] if args else None
         if query:
-            print(f"[LOG] Executing SQL Query: {query}")
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{timestamp}] Executing SQL Query: {query}")
         else:
             print("[LOG] No SQL query found.")
         return func(*args, **kwargs)
     return wrapper
 
-# Decorated function that executes a query
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
@@ -23,7 +24,7 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-# Run function and log output
+# Run function
 users = fetch_all_users(query="SELECT * FROM users")
 print(users)
 
