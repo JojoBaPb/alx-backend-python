db0fe601ceb8fea62ee29e23e0c1e98c25a41bbe
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-20 22:25:49 +0200

    Task 0: Project setup with Django and DRF, created chats app

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uy!&z%+b%5z0bq=kw_*j-atde@h22b)ta7nt31hzo820=c+w74'
+SECRET_KEY = 'django-insecure-4klcea$coedvs==em*l5hfli(3up1@bb0ro4))d(!lmmu)mp!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
