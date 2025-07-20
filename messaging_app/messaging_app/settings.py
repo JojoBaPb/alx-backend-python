7ad6de76dae533f8fe95d9aac93ee0c8be0f5107
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-20 22:29:01 +0200

    t0

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uy!&z%+b%5z0bq=kw_*j-atde@h22b)ta7nt31hzo820=c+w74'
+SECRET_KEY = 'django-insecure-qzgjm_(l!sjl7w=zt=6c)6mf^y0q2xhgu)1dli30&s2(l04ce('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
