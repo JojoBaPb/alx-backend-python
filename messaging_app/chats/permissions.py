632ed952221f0a9a5b634fe86f6cd9e60d97b8fe
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-25 21:50:39 +0200

    perm for task 2

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,7 +1,19 @@
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission, IsAuthenticated
+from .models import Conversation
 
-class IsParticipantOfConversation(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission to only allow participants of a conversation to access it.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
+        # obj can be a Message or Conversation depending on the view
+        try:
+            conversation = obj.conversation if hasattr(obj, 'conversation') else obj
+            return request.user in conversation.participants.all()
+        except AttributeError:
+            return False
 
