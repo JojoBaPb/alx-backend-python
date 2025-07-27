4c36dd72f37f5f22db8b03491345560e6b932f66
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-27 12:29:16 +0200

    logging class name changed

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,15 +1,10 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-class LogRequestMiddleware:
+class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Log the method and path of every incoming request
+        import logging
+        logger = logging.getLogger(__name__)
         logger.info(f"{request.method} request to {request.path}")
-
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)
 
