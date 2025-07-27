30305c8460c6b29bb965359dd46b83143cfefbcb
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-27 14:41:26 +0200

    t2 test 2

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,4 +1,5 @@
 import logging
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -9,3 +10,27 @@ class RequestLoggingMiddleware:
         logger.info(f"{request.method} request to {request.path}")
         return self.get_response(request)
 
+class AccessRestrictionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # List of allowed IP addresses
+        self.allowed_ips = ['127.0.0.1', 'localhost']
+
+    def __call__(self, request):
+        # Get client IP
+        ip = self.get_client_ip(request)
+
+        if ip not in self.allowed_ips:
+            return HttpResponseForbidden("Access Denied")
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        # Handle common proxy headers
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
