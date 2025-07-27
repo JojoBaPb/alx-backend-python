519b7f34e7433cc2a6b9eb00d394ad15382a6fb2
JojoBaPb <sipo.jmudenda@gmail.com>
2025-07-27 20:15:19 +0200

    t4 fix for checker

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers  # Import routers
+from rest_framework_nested import routers  # ✅ change this
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()  # Use routers.DefaultRouter() explicitly
+router = routers.NestedDefaultRouter()  # ✅ change this
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
