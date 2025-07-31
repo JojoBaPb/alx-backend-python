64de78e4938b04f8a8afa922454715757b43e7b4
JojoBaPb <sipo.jmudenda@gmail.com>
2025-08-01 01:05:32 +0200

    t1 checker fix

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited = models.BooleanField(default=False)  # Track if the message is edited
+
+    # NEW FIELDS
+    edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
 
     def __str__(self):
         return f'Message from {self.sender} to {self.receiver}'
