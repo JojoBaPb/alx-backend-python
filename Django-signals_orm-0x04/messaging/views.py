25e9cd131afc8fa3355884ae094912a413add042
JojoBaPb <sipo.jmudenda@gmail.com>
2025-08-03 20:15:21 +0200

    t3 checker test

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Prefetch
+from .models import Message
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def threaded_conversation_view(request, message_id):
+    message = Message.objects.select_related("sender", "receiver").prefetch_related("replies").get(id=message_id)
+    return render(request, "threaded_conversation.html", {"message": message})
+
+messages = Message.objects.select_related('sender').prefetch_related(
+    Prefetch('replies', queryset=Message.objects.select_related('sender'))
+).filter(conversation=some_conversation)
 
 @csrf_exempt
 def delete_user(request, user_id):
